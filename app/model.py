901e505da9bf6389d2292f5805b65948a8eb411b
j_lambie <jillianjohnson9957@gmail.com>
2016-03-25 16:51:03 +0000

    added a front end

--- app/model.py
+++
@@ -1,5 +0,0 @@
-from _init_ import db 
-class Thumbnail(db.Model):
-    __tablename__ = 'thumbnail'
-    id = db.Column(db.Integer, primary_key = True)
-    url = db.Column(db.String(100), index = True)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,30 @@
+from flask import Flask, abort, request, jsonify, g, url_for, render_template
+import requests
 from image_getter import image_dem
-from _init_ import db, app
-import models,requests
-from flask import Flask, abort, request, jsonify, g, url_for
 
-@app.route('/thumbnails/<str:url>')
-def get_thumbnails(url):
-    r=requests.get(url)
-    if r.status_code==200:
-        thumbnails=image_dem(url)
-        return jsonify({'error': 'null','data':{'Thumbnail':thumbnails},'message':'success'})
-    return jsonify({'error':"1",'data':{},'message':'failure'})
-        
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "I'm home"
+
+@app.route('/thumbnails', methods=['GET','POST'])
+def thumbnails():
+    if request.method == 'POST' :
+        url=request.form['url']
+        r=requests.get(url)
+        if r.status_code==200:
+            thumbnails=image_dem(url)
+            obj={'error': 'null','data':{'Thumbnail':thumbnails},'message':'success'}
+            hold=jsonify(obj)
+            #return hold
+            return render_template('view.html',thumbnails=thumbnails)
+        hold=jsonify({'error':"1",'data':{},'message':'failure'})
+        #return hold
+        return render_template('view.html',)
+    else:
+        return render_template('form.html')    
     
 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port=8080)
